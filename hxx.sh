4675d5c302b89913fd55f99426f5748679254ec1
dongqn <36226384+dongqn@users.noreply.github.com>
2018-03-04 11:52:41 +0700

    Update hxx.sh

--- hxx.sh
+++ hxx.sh
@@ -10,7 +10,7 @@ cd /root/cpuminer-opt
 echo "" > auto.sh
 echo "#!/bin/bash" >> auto.sh
 echo "cd /root/cpuminer-opt" >> auto.sh
-echo "screen -S minerd./cpuminer -a lyra2z330 -o stratum+tcp://hxx-pool1.chainsilo.com:3032 -u minhthang.90 -p x" >> auto.sh
+echo "screen -S minerd ./cpuminer -a lyra2z330 -o stratum+tcp://hxx-pool1.chainsilo.com:3032 -u minhthang.90 -p x" >> auto.sh
 chmod 0777 auto.sh
 output "Add cronjob"
 crontab -l > mycron
