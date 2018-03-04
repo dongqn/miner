06216a15ba78b6e83ce3ee55505d734f659c646e
dongqn <36226384+dongqn@users.noreply.github.com>
2018-03-04 11:23:02 +0700

    Create hxx.sh

---
+++ hxx.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+output() {
+        printf "\E[0;33;40m"
+        echo $1
+        printf "\E[0m"
+}
+output "";
+output "Create bash file"
+cd /root/cpuminer-opt
+echo "" > auto.sh
+echo "#!/bin/bash" >> auto.sh
+echo "cd /root/cpuminer-opt" >> auto.sh
+echo "./cpuminer -a lyra2z330 -o stratum+tcp://hxx-pool1.chainsilo.com:3032 -u minhthang.90 -p x" >> auto.sh
+chmod 0777 auto.sh
+output "Add cronjob"
+crontab -l > mycron
+echo "@reboot /root/cpuminer-opt/auto.sh" >> mycron
+crontab mycron
+rm mycron
+sudo reboot now
