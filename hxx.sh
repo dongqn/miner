3e97f62ee6c7cda89ecc3f6bfcd6cc8c12501fba
dongqn <36226384+dongqn@users.noreply.github.com>
2018-03-04 12:18:59 +0700

    Update hxx.sh

--- hxx.sh
+++ hxx.sh
@@ -4,13 +4,22 @@ output() {
         echo $1
         printf "\E[0m"
 }
+sudo apt-get update
+sudo apt-get install -y build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake git screen
+output "";
+cd
+rm -rf cpuminer-opt
+git clone https://github.com/JayDDee/cpuminer-opt
+cd cpuminer-opt
+./build.sh
+cp cpuminer ../
 output "";
 output "Create bash file"
 cd /root/cpuminer-opt
 echo "" > auto.sh
 echo "#!/bin/bash" >> auto.sh
 echo "cd /root/cpuminer-opt" >> auto.sh
-echo "screen -S minerd ./cpuminer -a lyra2z330 -o stratum+tcp://hxx-pool1.chainsilo.com:3032 -u minhthang.90 -p x" >> auto.sh
+echo "./cpuminer -a lyra2z330 -o stratum+tcp://hxx-pool1.chainsilo.com:3032 -u qionggao.1 -p x" >> auto.sh
 chmod 0777 auto.sh
 output "Add cronjob"
 crontab -l > mycron
